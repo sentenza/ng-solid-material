99a956ddaa6244531c76d7b3da4d79545eb8a08f
Alfredo <alfredotorre@gmail.com>
2018-10-08 20:01:25 +0100

    app.component and essential settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { Component, ViewChild } from '@angular/core'
 
 @Component({
   selector: 'app-root',
@@ -6,4 +6,10 @@ import { Component } from '@angular/core'
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  @ViewChild('sidenav') sidenavRef
+
+  toggleSidebar() {
+    this.sidenavRef.toggle()
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,29 +2,37 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms'
 import { MaterialModule } from './material/material.module'
 
+
 import { AppComponent } from './app.component'
-import { NavbarComponent } from './shared/navbar/navbar.component'
-import { FooterComponent } from './shared/footer/footer.component'
+import { NavbarComponent } from './layout/navbar/navbar.component'
+import { FooterComponent } from './layout/footer/footer.component'
 import { HomeComponent } from './home/home.component'
 import { AppRoutingModule } from './app-routing.module'
+import { LoginComponent } from './auth/login/login.component'
+import { RdfService } from './shared/rdf.service'
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
+    FormsModule,
     HttpClientModule,
     MaterialModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    RdfService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
