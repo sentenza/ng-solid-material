4bea01e6a22a19e6ee6be0b36feb8447b88a1e68
Alfredo <alfredotorre@gmail.com>
2018-10-10 15:45:01 +0100

    add environment app variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  app: {
+    version: '0.0.3',
+    repository: 'https://github.com/sentenza/ng-solid-material'
+  }
+}
 
 /*
  * In development mode, to ignore zone related error stack frames such as
